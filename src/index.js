f096a8084319da448f973fbb5534d7d168ed973b
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-22 03:49:49 +0900

    feat: youtube 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import GnbView from './View/GnbView';
 import View from './View/View';
+import YoutubeView from './View/YoutubeView';
 
 console.log('hello');
 new View().render();
 new GnbView().render();
+new YoutubeView().render();
