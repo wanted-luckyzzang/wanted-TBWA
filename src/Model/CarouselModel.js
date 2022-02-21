758abd708b77315e17364908c83f627db14ab8dd
안병진 <byungjin0120@gmail.com>
2022-02-21 22:33:00 +0900

    feat: CarouselView 추가

---
+++ src/Model/CarouselModel.js
@@ -0,0 +1,32 @@
+export default class CraouselModel {
+  constructor() {
+    this.craouselData = [
+      {
+        productName: '들꽃 화병꽃이(1/10~1/16)',
+        productPrice: '79,000원',
+        shopTag: [
+          '광화문점',
+          '잠실점',
+          '월계점',
+          '구로점',
+          '송파점',
+          '부산동래점',
+        ],
+        imageUrl: 'asdas',
+      },
+      {
+        productName: '롱앤로우 센터피스(1/17~1/23)',
+        productPrice: '79,000원',
+        shopTag: [
+          '광화문점',
+          '잠실점',
+          '월계점',
+          '구로점',
+          '송파점',
+          '부산동래점',
+        ],
+        imageUrl: 'asdas',
+      },
+    ];
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import CarouselView from './View/CarouselView';
 import View from './View/View';
 
 console.log('hello');
 new View().render();
+new CarouselView().render();
