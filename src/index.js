a8a16db2d3855bc0cd536c3f0e9fee386bb9aa04
minjuice1 <minjuice93@gmail.com>
2022-02-22 08:14:50 +0900

    design: chore: DoubleImgView로 파일 수정 및 Gnb style 수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import CarouselView from './View/CarouselView';
 import PageInfoView from './View/PageInfoView';
 import GnbView from './View/GnbView';
 import View from './View/View';
-import DoubleImg from './View/doubleImg';
+import DoubleImgView from './View/doubleImgView';
 import YoutubeView from './View/YoutubeView';
 
 console.log('hello');
 new View().render();
 new GnbView().render();
-new DoubleImg().render();
+new DoubleImgView().render();
 new PageInfoView().render();
 new YoutubeView().render();
