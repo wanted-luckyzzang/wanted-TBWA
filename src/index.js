edb37a56c9b2ed246d571845d145c475611ebc69
안병진 <byungjin0120@gmail.com>
2022-02-22 10:59:04 +0900

    refactor: console.log 제거

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import CarouselView from './View/CarouselView';
 import PageInfoView from './View/PageInfoView';
 import GnbView from './View/GnbView';
 import View from './View/View';
-import DoubleImg from './View/doubleImg';
+import DoubleImg from './View/DoubleImg';
 import YoutubeView from './View/YoutubeView';
 
-console.log('hello');
+import CarouselController from './Controller/CarouselController';
+
 new View().render();
 new GnbView().render();
 new DoubleImg().render();
 new PageInfoView().render();
 new YoutubeView().render();
+new CarouselController().clickEvent();
